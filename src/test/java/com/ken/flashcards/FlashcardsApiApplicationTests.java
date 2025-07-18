5d39ac8866a4d810645f3b200267083e7934f045
DragonSenses <kendrickfrancisco@gmail.com>
2025-07-18 14:13:10 -0700

    feat: Bootstrap Spring Boot w/ Maven

---
+++ src/test/java/com/ken/flashcards/FlashcardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ken.flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
