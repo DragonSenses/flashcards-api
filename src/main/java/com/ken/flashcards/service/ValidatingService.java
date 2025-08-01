0b0a2a5b0aa670c0b5e3500c4b1b10210692ccee
DragonSenses <kendrickfrancisco@gmail.com>
2025-08-01 16:47:22 -0700

    refactor: Update ValidatingService to use centralized exception messages
    - Replaced hardcoded strings with constants from ExceptionMessages
    - Applied format() for dynamic field name injection in assertNotBlank
    - Improves consistency and prepares codebase for future localization

--- src/main/java/com/ken/flashcards/service/ValidatingService.java
+++ src/main/java/com/ken/flashcards/service/ValidatingService.java
@@ -1,18 +1,30 @@
 package com.ken.flashcards.service;
 
+import static java.lang.String.format;
+
+import static com.ken.flashcards.constants.ExceptionMessages.FIELD_MUST_NOT_BE_BLANK;
+import static com.ken.flashcards.constants.ExceptionMessages.REQUEST_BODY_NULL;
 import com.ken.flashcards.exception.BadRequestException;
 
 public abstract class ValidatingService {
 
+  /**
+   * Throws BadRequestException if the object is null.
+   */
   protected void assertNotNull(Object object) {
     if (object == null) {
-      throw new BadRequestException("Request body must not be null");
+      throw new BadRequestException(REQUEST_BODY_NULL);
     }
   }
 
+  /**
+   * Throws BadRequestException if the string is null or blank.
+   * 
+   * @param fieldName used in the exception message
+   */
   protected void assertNotBlank(String value, String fieldName) {
     if (value == null || value.trim().isEmpty()) {
-      throw new BadRequestException(fieldName + " must not be blank");
+      throw new BadRequestException(format(FIELD_MUST_NOT_BE_BLANK, fieldName));
     }
   }
 
