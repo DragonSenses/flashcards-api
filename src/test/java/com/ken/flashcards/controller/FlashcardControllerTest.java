cbed384acd6d80a6f55edc2fc6d2e708b642c805
DragonSenses <kendrickfrancisco@gmail.com>
2025-08-08 17:56:37 -0700

    test(flashcard-controller): Return 200 when retrieving all flashcards

--- src/test/java/com/ken/flashcards/controller/FlashcardControllerTest.java
+++ src/test/java/com/ken/flashcards/controller/FlashcardControllerTest.java
@@ -1,11 +1,19 @@
 package com.ken.flashcards.controller;
 
+import java.util.Set;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.ken.flashcards.model.Flashcard;
 import com.ken.flashcards.service.FlashcardService;
@@ -34,5 +42,14 @@ public class FlashcardControllerTest extends ControllerTestBase {
   void setup() {
     this.flashcard = new Flashcard(expectedFlashcardId, expectedStudySessionId, expectedQuestion,
         expectedAnswer);
+  }
+
+  @DisplayName("GET /flashcards - should return 200 with all flashcards")
+  @Test
+  void shouldReturn200WithAllFlashcards() throws Exception {
+    when(flashcardService.findAll()).thenReturn(Set.of(flashcard));
+
+    mockMvc.perform(get(flashcardsPath)).andExpect(status().isOk())
+        .andExpect(content().json(serialize(Set.of(flashcard))));
   }
 }
