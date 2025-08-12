9102448a0f724b2e712f1a37eb7049157ba92e6b
DragonSenses <kendrickfrancisco@gmail.com>
2025-08-12 13:11:21 -0700

    test: Inject category path from application properties
    Adds `@Value` injection for `${spring.servlet.path.categories}` to dynamically resolve endpoint path.

--- src/test/java/com/ken/flashcards/integration/CategoryIntegrationTest.java
+++ src/test/java/com/ken/flashcards/integration/CategoryIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.ken.flashcards.integration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -12,6 +13,10 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class CategoryIntegrationTest {
+
+  @Value("${spring.servlet.path.categories}")
+  private String path;
+
   @Autowired
   private WebTestClient client;
 }
